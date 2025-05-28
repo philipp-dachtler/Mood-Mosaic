2da9d9175618a636ffd942cff81bea0e8e6300c0
philipp-dachtler <philippdachtler@proton.me>
2025-05-28 15:09:42 +0200

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("mood-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
